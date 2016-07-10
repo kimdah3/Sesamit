04831fb6b1239d4d9697cb2fbf4d6471e1905810
johnsrsly <johnelarssonz@gmail.com>
2016-07-10 21:07:46 +0200

    En början på sidan "vad är sesam?"

--- src/Sesamit/Controllers/HomeController.cs
+++ src/Sesamit/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace Sesamit.Controllers
             return View();
         }
 
+        public IActionResult VadArSesam()
+        {
+            return View();
+        }
+
         public IActionResult Error()
         {
             return View();
