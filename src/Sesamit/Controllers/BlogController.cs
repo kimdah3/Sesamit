87bf670bd299add33203b0de0bafb10ad255ecf4
kimdah3 <dahlberg.kim20@gmail.com>
2016-07-27 12:17:33 +0200

    Blogg

--- src/Sesamit/Controllers/BlogController.cs
+++ src/Sesamit/Controllers/BlogController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel.Syndication;
 using System.Threading.Tasks;
+using System.Xml;
 using Microsoft.AspNetCore.Mvc;
+using Sesamit.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,7 +16,26 @@ namespace Sesamit.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            var url = "http://sesamnu.blogg.se/index.rss";
+            var bloggModel = new BloggRssModel();
+
+            using (var reader = XmlReader.Create(url))
+            {
+                var feed = SyndicationFeed.Load(reader);
+                foreach (var item in feed.Items)
+                {
+                    var bloggPost = new BloggPost()
+                    {
+                        Title = item.Title.Text,
+                        Description = item.Summary.Text.Substring(0, item.Summary.Text.IndexOf('-')),
+                        Link = item.Links[0].Uri,
+                        PubDate = DateTime.Parse(item.PublishDate.ToString())
+                    };
+                    bloggModel.BloggPosts.Add(bloggPost);
+                }
+            }
+            return View(bloggModel);
         }
     }
+
 }
---
+++ src/Sesamit/Models/BloggModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sesamit.Models
+{
+    public class BloggRssModel
+    {
+        public List<BloggPost> BloggPosts { get; set; }
+
+        public BloggRssModel()
+        {
+            BloggPosts = new List<BloggPost>();
+        }
+    }
+
+    public class BloggPost
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime PubDate { get; set; }
+        public Uri Link { get; set; }
+    }
+}
