3b4e8ef8deb615c0d6d4b22dd9ef0a77f696f46d
kimdah3 <dahlberg.kim20@gmail.com>
2016-08-05 12:26:12 +0200

    campusmässan

--- src/Sesamit/Controllers/OmSesamController.cs
+++ src/Sesamit/Controllers/OmSesamController.cs
@@ -25,5 +25,10 @@ namespace Sesamit.Controllers
         {
             return View();
         }
+
+        public IActionResult Campusmassan()
+        {
+            return View();
+        }
     }
 }
