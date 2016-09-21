1802f55f22431e4129de04bc306c52d2fb050430
kimdah3 <dahlberg.kim20@gmail.com>
2016-09-21 19:11:48 +0200

    småfix och nyhetssidan

--- src/Sesamit/wwwroot/js/site.js
+++ src/Sesamit/wwwroot/js/site.js
@@ -33,3 +33,14 @@ $(function () {
 
 });
 // IMPORTERAD NEWS CAROUSEL
+
+
+//Facebook
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/sv_SE/sdk.js#xfbml=1&version=v2.7&appId=107579145685";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
