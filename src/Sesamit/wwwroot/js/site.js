3f2d3b28f8d858c1ad1e2b332f9cceee4c99ec56
Oliver Sundbäck <oliver.sundback93@gmail.com>
2017-06-25 19:54:04 +0200

    Scroll to top
    Added scroll to top

--- src/Sesamit/wwwroot/js/site.js
+++ src/Sesamit/wwwroot/js/site.js
@@ -9,6 +9,30 @@ $(function () {
         });
 });
 
+//Scroll to top
+
+$(function () {
+
+    $(document).on('scroll', function () {
+
+        if ($(window).scrollTop() > 100) {
+            $('.scroll-top-wrapper').addClass('show');
+        } else {
+            $('.scroll-top-wrapper').removeClass('show');
+        }
+    });
+
+    $('.scroll-top-wrapper').on('click', scrollToTop);
+});
+
+function scrollToTop() {
+    verticalOffset = typeof (verticalOffset) != 'undefined' ? verticalOffset : 0;
+    element = $('body');
+    offset = element.offset();
+    offsetTop = offset.top;
+    $('html, body').animate({ scrollTop: offsetTop }, 500, 'linear');
+}
+
 // Google maps
 var map;
 function initMap() {
